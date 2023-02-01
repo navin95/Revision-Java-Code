3ce0c41ea378f02454ffe1f44a577038b2b0128e
navin95 <navinvijayramkumar@gmail.com>
2023-02-01 12:25:28 +0530

    Initial program 1

---
+++ src/org/basic/ProgramOne.java
@@ -0,0 +1,11 @@
+package org.basic;
+
+// print numbers from 1 to 100 without using any numbers
+public class ProgramOne {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+		      System.out.println(i);
+		    }
+	}
+
+}
